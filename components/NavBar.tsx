688b108e30c6d3daf80b8dccbb93bc7c07a105c2
Aniruddha Rawool <aniruddha.212074101@vcet.edu.in>
2024-06-25 09:41:16 +0530

    navbar done

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import { NavLinks } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import AuthProvider from "./AuthProvider";
 
 const NavBar = () => {
+  const session = {};
   return (
-    <div>
-      NAvBar
-    </div>
-  )
-}
+    <nav className="flexBetween navbar">
+      <div className="flex-1 flexStart gap-10">
+        <Link href="/">
+          <Image src="/logo.svg" alt="flexibble" width={115} height={43} />
+        </Link>
+        <ul className="xl:flex hidden text-small gap-7">
+          {NavLinks.map((link) => (
+            <Link key={link.key} href={link.href}>
+              {link.text}
+            </Link>
+          ))}
+        </ul>
+      </div>
+      <div className="flexCenter gap-4">
+        {session ? (
+          <>
+            UserPhoto
+            <Link href="/create-project">Share Work</Link>
+          </>
+        ) : (
+          <>
+          <AuthProvider/></>
+        )}
+      </div>
+    </nav>
+  );
+};
 
-export default NavBar
+export default NavBar;
